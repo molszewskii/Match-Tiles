6b2fb9c16c6258591c59ff09c5369b953b609b24
Michał <80591@student.pb.edu.pl>
2024-10-31 18:32:17 +0100

    Add LoadingSpinner component for displaying loading state

---
+++ src/components/loadingSpinner.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './../styles/loadingSpinner.scss';
+
+export const LoadingSpinner: React.FC = () => {
+    return (
+        <div className='spinner-container'>
+            <div className="loading-spinner">
+            </div>
+        </div>
+        
+    );
+};
+
