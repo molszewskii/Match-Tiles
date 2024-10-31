8f6b213f99328176f408ad85b501ed324ceef293
Michał <80591@student.pb.edu.pl>
2024-10-31 18:26:24 +0100

    Add assets folder with images and resources

---
+++ src/assets/christmas/index.tsx
@@ -0,0 +1,29 @@
+import ChristmasLights1 from './012-christmas-lights-1.png';
+import Gift from './011-gift.png';
+import Sleigh from './010-sleigh.png';
+import ChristmasLights from './009-christmas-lights.png';
+import Star from './008-star.png';
+import Bauble from './007-bauble.png';
+import SantaClaus from './006-santa-claus.png';
+import Reindeer from './005-reindeer.png';
+import GingerbreadMan from './004-gingerbread-man.png';
+import ChristmasBell from './003-christmas-bell.png';
+import Hat from './002-hat.png';
+import ChristmasWreath from './001-christmas-wreath.png';
+
+const ChristmasImages = [
+    ChristmasLights1,
+    Gift,
+    Sleigh,
+    ChristmasLights,
+    Star,
+    Bauble,
+    SantaClaus,
+    Reindeer,
+    GingerbreadMan,
+    ChristmasBell,
+    Hat,
+    ChristmasWreath,
+];
+
+export default ChristmasImages;
---
+++ src/assets/halloween/index.tsx
@@ -0,0 +1,29 @@
+import Bat from './bat.png';
+import BlackCat from './black-cat.png';
+import CandyBag from './candy-bag.png';
+import Dracula from './dracula.png';
+import GhostFace from './ghost-face.png';
+import Ghost from './ghost.png';
+import HauntedHouse from './haunted-house.png';
+import Monster from './monster.png';
+import Pumpkin from './pumpkin.png';
+import Skeleton from './skeleton.png';
+import SpiderWeb from './spider-web.png';
+import WitchHat from './witch-hat.png';
+
+const HalloweenImages = [
+    Bat,
+    BlackCat,
+    CandyBag,
+    Dracula,
+    GhostFace,
+    Ghost,
+    HauntedHouse,
+    Monster,
+    Pumpkin,
+    Skeleton,
+    SpiderWeb,
+    WitchHat
+];
+
+export default HalloweenImages;
