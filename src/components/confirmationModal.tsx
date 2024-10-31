b5eb69e60721268c194586710c723f09049a23f7
Michał <80591@student.pb.edu.pl>
2024-10-31 18:50:29 +0100

    Added '*.tsbuildinfo' to .gitignore and fixed import in confirmationModal.tsx

--- src/components/confirmationModal.tsx
+++ src/components/confirmationModal.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Button } from './button';
 import ReactDOM from "react-dom";
-import './../styles/confirmationModal.scss';
+import './../styles/confirmationModaL.scss';
 
 interface ConfirmationModalProps {
     isOpen: boolean;
